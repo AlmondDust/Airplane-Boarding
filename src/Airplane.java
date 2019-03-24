8341c5a8d78ce3b4eb36183b86ce355bf589bd01
Adam Buchinsky <asb153@pitt.edu>
2019-03-24 02:55:50 -0400

    Added a bit to a bunch, nothing in particular

--- src/Airplane.java
+++ src/Airplane.java
@@ -1,10 +1,45 @@
+import java.io.*;
+import java.util.stream.*;
+
 public class Airplane {
 
+    private static final int ROW_SPACING = 20;
+    private static final int COL_SPACING = 20;
+
+    private int[] seatPattern;
+    private int rows;
+    private int cols;
+
+
+
+    Seat[][] seats;
+    Passenger[] passengers;
+
     public Airplane(int rows, int ...seatPattern){
+        this.rows = rows;
+        this.seatPattern = seatPattern;
+
+        //Get number of cols
+        cols = IntStream.of(seatPattern).sum();
+
+        this.seats = new Seat[rows][cols];
+        this.passengers = new Passenger[numSeats()];
+
+        for(int row = 0; row < seats.length; row++){
+            for(int col = 0; col < seats[0].length; col++){
+                seats[row][col] = new Seat(row, col);
+                passengers[row*cols + col] = new Passenger(seats[row][col]);
+            }
+        }
+
 
     }
 
     public Airplane(String FileName){
 
+    }
+
+    private int numSeats(){
+        return rows*cols;
     }
 }
--- src/Passenger.java
+++ src/Passenger.java
@@ -1,2 +1,34 @@
 public class Passenger {
+
+    int[] coordinate = new int[2];
+
+    Seat seat;
+    boolean seated;
+    boolean hasBag;
+
+    float angle;
+
+    public Passenger(Seat seat){
+        this.seat = seat;
+    }
+
+    public int[] getCoordinate() {
+        return coordinate;
+    }
+
+    public Seat getSeat() {
+        return seat;
+    }
+
+    public boolean isSeated() {
+        return seated;
+    }
+
+    public boolean isHasBag() {
+        return hasBag;
+    }
+
+    public float getAngle() {
+        return angle;
+    }
 }
